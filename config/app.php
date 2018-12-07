0e14de46be303f46f7ad0a6088875f90ef9697ac
lvtianxinyuan <32013546+lvtianxinyuan@users.noreply.github.com>
2018-12-07 23:45:39 +0800

    添加Voyager服务提供程序

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        TCG\Voyager\VoyagerServiceProvider::class,
+
     ],
 
     /*
